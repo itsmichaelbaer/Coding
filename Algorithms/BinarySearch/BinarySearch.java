013c4527393d9c13d8e6ab29bd2c692fbbe51a8b
Michael Menaker <38389576+itsmichaelbaer@users.noreply.github.com>
2018-04-14 23:28:06 -0500

    Update BinarySearch.java

--- Algorithms/BinarySearch/BinarySearch.java
+++ Algorithms/BinarySearch/BinarySearch.java
@@ -5,15 +5,15 @@ public class BinarySearch
 //iterative method  
 public static int searchIterative(int[] arr, int target)
 {
-  int left=0, right=arr.length-1;
+  int left = 0, right = arr.length-1;
   
-  while(left<=right)
+  while(left <= right)
   {
-    int mid=left+(right-left)/2
+    int mid = left + (right - left) / 2
       
-    if (target<arr[mid]) right=mid-1;
+    if (target < arr[mid]) right = mid -1;
     
-    else if (target>arr[mid]) left=mid+1;
+    else if (target > arr[mid]) left = mid + 1;
     
     else return mid;
   }
@@ -23,19 +23,19 @@ public static int searchIterative(int[] arr, int target)
 //recursive method
 public static int searchRecursive(int[] arr, int left, int right, int target)
 {
-  if(left>right)
+  if(left > right)
     return -1;
   
-  int mid=left+(right-left)/2
+  int mid = left + (right - left) / 2
     
-  if(target==arr[mid])
+  if(target == arr[mid])
     return mid;
   
-  if(target<arr[mid])
+  if(target < arr[mid])
     return searchRecursive(arr, left, mid-1, target);
   
-  if(target>arr[mid])
-    return searchRecursive(arr, mid+1, right, target);
+  if(target > arr[mid])
+    return searchRecursive(arr, mid + 1, right, target);
 }
 
 }
